79128702a576b6ea8c6f6bb97df0a4e774ce0a8f
Refered <jta.ordo@gmail.com>
2023-10-02 20:11:24 -0600

    movimiento original

--- Assets/Scripts/Movimiento_Chamaco1.cs
+++ Assets/Scripts/Movimiento_Chamaco1.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movimiento_Chamaco1 : MonoBehaviour
-
 {
-    [SerializeField]
-    public float velocidadMovimiento = 2f; 
-    public float fuerzaSalto = 3.0f;
-    public float velocidadCorrer = 10f;
+    private float velocidadMovimiento = 2.2f;
+    private float fuerzaSalto = 7f;
+    private float velocidadCorrer = 4.5f;
+    private float velocidadEmpujando = 1.2f;
+    public float distanciaRaycast = 1.0f;
     private Rigidbody2D rb;
     private bool enSuelo = true;
-    private bool interactuable = true; 
-    private float tiempoDeEspera = 3.0f;
-
+    private bool tocandoObjetoEmpujable = false;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -21,10 +19,48 @@ public class Movimiento_Chamaco1 : MonoBehaviour
 
     private void Update()
     {
+        Debug.Log(rb.velocity);
+        Debug.Log(enSuelo);
+        Debug.Log(tocandoObjetoEmpujable);
+        movement();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            enSuelo = true;
+
+        }
+
+        if (collision.gameObject.CompareTag("Empujable"))
+        {
+            tocandoObjetoEmpujable = true;
+        }
+        else
+        {
+            tocandoObjetoEmpujable = false;
+        }
+    }
+
+    private void movement()
+    {
+
         float movimientoHorizontal = Input.GetAxis("Horizontal");
         Vector2 movimiento = new Vector2(movimientoHorizontal, 0);
-        rb.velocity = new Vector2(movimiento.x * velocidadMovimiento, rb.velocity.y);
-        if (enSuelo && Input.GetKey(KeyCode.LeftShift))
+
+        if (tocandoObjetoEmpujable)
+        {
+            rb.velocity = new Vector2(movimiento.x * velocidadEmpujando, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(movimiento.x * velocidadMovimiento, rb.velocity.y);
+
+
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             rb.velocity = new Vector2(movimiento.x * velocidadCorrer, rb.velocity.y);
         }
@@ -34,16 +70,9 @@ public class Movimiento_Chamaco1 : MonoBehaviour
         }
         if (enSuelo && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
-            enSuelo = false;
-        }
-    }
+            rb.AddForce(new Vector2(1f, 1f).normalized * fuerzaSalto, ForceMode2D.Impulse);
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Suelo"))
-        {
-            enSuelo = true;
+            enSuelo = false;
         }
     }
 }
