a46738ab3b01040e3923ae30cb446cb9e8459d76
DanielKyjas <dani.kyjas@hotmail.com>
2023-10-03 19:50:11 -0600

    Update Demonio1.cs

--- Assets/Scripts/Demonio1.cs
+++ Assets/Scripts/Demonio1.cs
@@ -8,21 +8,26 @@ public class Demonio1 : MonoBehaviour
     public float velocidadHorizontal = 3f;
     public Transform player;
     private bool movimientoDetenido = true;
+
     public float distanciaCampoVision = 5f; 
+
     private Vector3 direccionRayoDerecha; 
     private Vector3 direccionRayoIzquierda; 
+
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        direccionRayoDerecha = Vector2.right * distanciaCampoVision;
-        direccionRayoIzquierda = Vector2.left * distanciaCampoVision;
+        direccionRayoDerecha = Vector3.right * distanciaCampoVision;
+        direccionRayoIzquierda = Vector3.left * distanciaCampoVision;
     }
 
     private void Update()
     {
         if (!movimientoDetenido)
         {
+
             RaycastHit2D hitDerecha = Physics2D.Raycast(transform.position, direccionRayoDerecha, distanciaCampoVision, LayerMask.GetMask("Chamaco"));
             RaycastHit2D hitIzquierda = Physics2D.Raycast(transform.position, direccionRayoIzquierda, distanciaCampoVision, LayerMask.GetMask("Chamaco"));
 
@@ -39,6 +44,7 @@ public class Demonio1 : MonoBehaviour
 
         Debug.DrawRay(transform.position, direccionRayoDerecha, Color.red);
         Debug.DrawRay(transform.position, direccionRayoIzquierda, Color.red);
+
     }
 
     public void CambioMovimiento()
