c950e93f0900edcb512364f4ae985510d05a9aa2
DanielKyjas <dani.kyjas@hotmail.com>
2023-10-03 18:11:13 -0600

    Campo de vision
    Agregar campo de vision al demonio

--- Assets/Scripts/Demonio1.cs
+++ Assets/Scripts/Demonio1.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class Demonio1 : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float velocidadHorizontal = 3f; 
+    public float velocidadHorizontal = 3f;
     public Transform player;
-    private bool movimientoDetenido = false;
+    private bool movimientoDetenido = true;
+    public float distanciaCampoVision = 5f; 
+    private Vector3 direccionRayoDerecha; 
+    private Vector3 direccionRayoIzquierda; 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        direccionRayoDerecha = Vector2.right * distanciaCampoVision;
+        direccionRayoIzquierda = Vector2.left * distanciaCampoVision;
     }
 
     private void Update()
     {
         if (!movimientoDetenido)
         {
-            float direccionHorizontal = Mathf.Sign(player.position.x - transform.position.x);
-            rb.velocity = new Vector2(direccionHorizontal * velocidadHorizontal, rb.velocity.y);
+            RaycastHit2D hitDerecha = Physics2D.Raycast(transform.position, direccionRayoDerecha, distanciaCampoVision, LayerMask.GetMask("Chamaco"));
+            RaycastHit2D hitIzquierda = Physics2D.Raycast(transform.position, direccionRayoIzquierda, distanciaCampoVision, LayerMask.GetMask("Chamaco"));
+
+            if (hitDerecha.collider != null || hitIzquierda.collider != null)
+            {
+                Vector2 direccionHaciaChamaco = (player.position - transform.position).normalized;
+                rb.velocity = new Vector2(direccionHaciaChamaco.x * velocidadHorizontal, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
         }
+
+        Debug.DrawRay(transform.position, direccionRayoDerecha, Color.red);
+        Debug.DrawRay(transform.position, direccionRayoIzquierda, Color.red);
     }
 
     public void CambioMovimiento()
