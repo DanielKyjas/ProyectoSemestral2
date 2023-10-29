28a53f33dcfae086ee70d4555befbe6cb83980f5
DanielKyjas <dani.kyjas@hotmail.com>
2023-10-29 14:01:07 -0600

    Pausa y avanzando en el sistema e textos

---
+++ Assets/Scripts/UI/Text.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Text 
+{
+    [TextArea(2, 6)]
+    public string[] arrayTextos;
+}
