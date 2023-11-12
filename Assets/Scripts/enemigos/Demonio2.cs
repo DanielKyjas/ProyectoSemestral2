ffda7c345e12c122bc0b1e87e7c12ece3a647c67
Refered <jta.ordo@gmail.com>
2023-11-12 15:09:57 -0600

    Animacion arana con errores
    todavia no tiene la logica la animacion

--- Assets/Scripts/enemigos/Demonio2.cs
+++ Assets/Scripts/enemigos/Demonio2.cs
@@ -19,11 +19,14 @@ private Rigidbody2D rb;
     private BoxCollider2D boxCollider;
     private SpriteRenderer spriteRenderer;
     private bool mirandoDerecha = true;
+    private bool bajando = false;
+    private bool siguiendo = false;
+    private Animator animator;
     [SerializeField] private new GameObject light;
 
     private void Start()
     {
-
+        animator = GetComponent<Animator>();   
         rb = GetComponent<Rigidbody2D>();
         direccionRayoDerecha = Vector2.right * distanciaCampoVision;
         direccionRayoIzquierda = Vector2.left * distanciaCampoVision;
@@ -35,12 +38,14 @@ private Rigidbody2D rb;
 
     private void Update()
     {
+        animator.SetBool("movimientodetenido",movimientoDetenido);
+        animator.SetBool("bajando",bajando);
         Vector2 puntoInicial = new(transform.position.x, transform.position.y - 2.0f);
         RaycastHit2D hitAbajo2 = Physics2D.Raycast(puntoInicial, direccionRayoAbajo, distanciaCampoVision2, LayerMask.GetMask("Objeto"));
-         Vector2 puntoAbajo = transform.position - new Vector3(0, 0.5f);
+        Vector2 puntoAbajo = transform.position - new Vector3(0, 0.5f);
         if (hitAbajo2.collider != null)
         {
-             boxCollider = hitAbajo2.collider.GetComponent<BoxCollider2D>();
+            boxCollider = hitAbajo2.collider.GetComponent<BoxCollider2D>();
             if (boxCollider != null)
             {
                 boxCollider.isTrigger = false;
@@ -53,6 +58,8 @@ private Rigidbody2D rb;
         }
         if (movimientoDetenido)
         {
+            bajando = false;
+            siguiendo = false;
             if (!tocoTecho)
             {
                 rb.constraints = RigidbodyConstraints2D.FreezePositionX;
@@ -67,6 +74,7 @@ private Rigidbody2D rb;
 
         if (!movimientoDetenido)
         {
+
             light.SetActive(false);
             RaycastHit2D hitDerecha = Physics2D.Raycast(transform.position, direccionRayoDerecha, distanciaCampoVision, LayerMask.GetMask("Chamaco", "Piedra"));
             RaycastHit2D hitIzquierda = Physics2D.Raycast(transform.position, direccionRayoIzquierda, distanciaCampoVision, LayerMask.GetMask("Chamaco", "Piedra"));
@@ -75,13 +83,14 @@ private Rigidbody2D rb;
             RaycastHit2D hitIzquierdaAbajo = Physics2D.Raycast(puntoAbajo, direccionRayoIzquierda, distanciaCampoVision, LayerMask.GetMask("Chamaco", "Piedra"));
             if (hitAbajo.collider != null)
             {
+                bajando = true;
                 rb.constraints = RigidbodyConstraints2D.FreezePositionX;
                 rb.velocity = new Vector2(rb.velocity.x, -5f);
                 tocoTecho = false;
             }
+            else { bajando =  false; }
             if (hitDerecha.collider != null || hitIzquierda.collider != null || hitDerechaAbajo.collider != null || hitIzquierdaAbajo.collider != null)
             {
-
                 rb.constraints = RigidbodyConstraints2D.None;
                 Vector2 direccionHaciaChamaco = (player.position - transform.position).normalized;
                 rb.velocity = new Vector2(direccionHaciaChamaco.x * velocidadHorizontal, rb.velocity.y);
@@ -89,6 +98,7 @@ private Rigidbody2D rb;
             else
             if ((hitDerecha.collider == null || hitIzquierda.collider == null || hitDerechaAbajo.collider != null || hitIzquierdaAbajo.collider != null) && tocosuelo == true)
             {
+                siguiendo = false;
                 rb.constraints = RigidbodyConstraints2D.FreezePositionX;
                 rb.velocity = new Vector2(rb.velocity.x, 5f);
             }
@@ -105,6 +115,7 @@ private Rigidbody2D rb;
 
         if (collision.gameObject.CompareTag("Suelo"))
         {
+            siguiendo = true;
             tocosuelo = true;
         }
         if (collision.gameObject.CompareTag("Techo"))
