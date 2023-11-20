9a39c8b1a8715c0040eedc608e54bd6ca5fc8de0
DanielKyjas <dani.kyjas@hotmail.com>
2023-11-20 00:26:30 -0600

    La configuracion terminada
    mas o menos porque aun falta lo del volumen pero aun no hay sonidos.XD

--- Assets/Scripts/UI/Pause.cs
+++ Assets/Scripts/UI/Pause.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Pause : MonoBehaviour
 {
     [SerializeField] private GameObject pausebotton;
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject startMenu;
     private bool juegoPausado = false;
-
+    public static Pause instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -35,6 +48,13 @@ public class Pause : MonoBehaviour
         Time.timeScale = 1f;
         pausebotton.SetActive(true);
         pauseMenu.SetActive(false);
+    }
+    public void Regresar()
+    {
+        SceneManager.LoadScene("Start Menu");
+        pausebotton.SetActive(false);
+        startMenu.SetActive(true);
+ 
     }
     public void Cerrar()
     {
--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -2,16 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
+    public static StartMenu instance;
+    [SerializeField] private GameObject pause;
+    [SerializeField] private GameObject startMenu;
+    [SerializeField] private Slider sliderProgress;
+    private bool startload = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (startload)
+        {
+            StartCoroutine(Load());
+            startload = false;
+           
+        }
+    }
+
     public void Jugar()
     {
-        SceneManager.LoadScene("Vestibulo");
+       
+        startload = true;
     }
+
     public void Quit()
     {
         Debug.Log("Salir");
-        Application.Quit(); 
+        Application.Quit();
+    }
+
+    IEnumerator Load()
+    {
+        sliderProgress.gameObject.SetActive(true);
+        AsyncOperation load = SceneManager.LoadSceneAsync("Vestibulo");
+
+        while (load.isDone == false)
+        {
+            float progress = Mathf.Clamp01(load.progress / 0.9f);
+            sliderProgress.value = progress;
+            yield return null;
+        }
+        if(load.isDone == true)
+        {
+            pause.SetActive(true);
+            startMenu.SetActive(false);
+            sliderProgress.gameObject.SetActive(false);
+        }
     }
 }
